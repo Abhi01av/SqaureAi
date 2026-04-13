890b2611b8d6864a6fdba56d6986856913cfaa06
sandeshATH <luccky249@gmail.com>
2026-04-13 17:32:46 +0530

    final done

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,11 @@
-import logoImg from '../assests/logonew.jpeg';
+import logoMark from '../assests/neww.svg';
 import './Logo.css';
 
 const Logo = ({ size = "medium", hideText = false }) => {
   return (
     <div className={`logo-wrapper logo-${size}`}>
       <div className={`logo-img-container logo-img-${size}`}>
-        <img src={logoImg} alt="Dot Square Automation" className="logo-img" />
+        <img src={logoMark} alt="Dot Square Automation" className="logo-img" />
       </div>
 
       {!hideText && (
